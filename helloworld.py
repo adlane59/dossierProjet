272ca93cfc6900f06770ce2d1177ea705a436b2b
adlane59 <adlene2606@hotmail.com>
2023-03-13 13:13:51 +0100

    initialisation 13/03/2023 à 13:13

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrateur
+#
+# Created:     13/03/2023
+# Copyright:   (c) Administrateur 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+str = "hello world"
+print (str)
